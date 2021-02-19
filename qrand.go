3d85bbab1789648d60dde0da423e93d0b0dadfb1
Ishuah Kariuki <kariuki@ishuah.com>
2021-02-19 14:01:09 +0300

    Feature: Read func

--- qrand.go
+++ qrand.go
@@ -21,3 +21,32 @@ func Int() (int, error) {
 
 	return int(i), nil
 }
+
+func Read(p []byte) (n int, err error) {
+	size := len(p)
+	length := 1
+	if len(p) > 1024 {
+		size = 1024
+		length = len(p) / 1024
+	}
+
+	response, err := Get(length, "hex16", size)
+
+	if err != nil {
+		return
+	}
+
+	var data string
+
+	for _, value := range response.Data {
+		str, _ := value.(string)
+		data += str
+	}
+
+	for n = 0; n < len(p); n++ {
+		val, _ := strconv.ParseInt(data[n*2:(n*2)+2], 16, 64)
+		p[n] = byte(val)
+	}
+
+	return
+}
