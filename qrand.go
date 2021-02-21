e7e1137602011a533db0227f32a4f9334a9fe21a
Ishuah Kariuki <kariuki@ishuah.com>
2021-02-21 18:07:59 +0300

    First stab at error handling in the generator

--- qrand.go
+++ qrand.go
@@ -1,10 +1,9 @@
 package qrand
 
-// import (
-// 	"strconv"
-// )
-
-// Generator!
+import (
+	"fmt"
+	"time"
+)
 
 var (
 	qrand Qrand
@@ -17,38 +16,76 @@ func init() {
 
 type Qrand struct {
 	stream chan float64
+	err    error
+}
+
+func (q *Qrand) retry() {
+	for {
+		time.Sleep(time.Second)
+		_, err := Get(1, "uint16", 1)
+		if err != nil {
+			fmt.Println("The struggle is real")
+			continue
+		}
+
+		q.err = nil
+		go q.Generator()
+		return
+	}
 }
 
 func (q *Qrand) Generator() {
 	for {
-		response, _ := Get(1024, "uint16", 1)
+		response, err := Get(1024, "uint16", 1)
+
+		if err != nil {
+			q.err = err
+			fmt.Println("calling q.retry")
+			go q.retry()
+			return
+		}
+
 		for _, value := range response.Data {
 			i, _ := value.(float64)
 			q.stream <- i
 		}
 	}
-	close(q.stream)
+	defer close(q.stream)
 }
 
-func (q *Qrand) Int() int {
+func (q *Qrand) Int() (int, error) {
+	if q.err != nil {
+		return 0, q.err
+	}
+
 	i := <-q.stream
-	return int(i)
+	return int(i), nil
 }
 
-func (q *Qrand) Byte() byte {
+func (q *Qrand) Byte() (byte, error) {
+	if q.err != nil {
+		return byte(0), q.err
+	}
+
 	b := <-q.stream
-	return byte(b)
+	return byte(b), nil
 }
 
-func Int() int {
-	i := qrand.Int()
-	return int(i)
+func Int() (int, error) {
+	i, err := qrand.Int()
+	return int(i), err
 }
 
 func Read(p []byte) (n int, err error) {
 
 	for n = 0; n < len(p); n++ {
-		p[n] = qrand.Byte()
+		b, err := qrand.Byte()
+
+		if err != nil {
+			return 0, err
+		}
+
+		p[n] = b
 	}
 
 	return
